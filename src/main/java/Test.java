ac431498b5822d86b1839f342904f75250c8fe3a
Maksym Shyiko <maxymshyjko@gmail.com>
2022-01-17 14:30:33 +0200

    Added usage of dependency from parent pom

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,11 @@
+import org.springframework.beans.BeanMetadataAttribute;
 
 public class Test {
     public static void main(String[] args) {
         PC pc = new PC("Asus", new PCProcessor(2.8), 32);
         System.out.println(pc);
+
+        //Dependency from parent pom
+        System.out.println(BeanMetadataAttribute.class.getCanonicalName());
     }
 }
